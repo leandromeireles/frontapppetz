acc44beba4a2ee2e05bfac998c9416f3b2da6c14
Leandro Meireles <lmmeireles11@gmail.com>
2021-01-19 18:12:47 -0300

    Ajustes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT  =  process.env.PORT || 8080;
+app.use(express.static(__dirname + '/dist/petzappfront'));
+
+
+app.get('/', function(req,res) {
+
+    res.sendFile(path.join(__dirname+'/dist/petzappfront/index.html'));
+    });
+    
+    app.listen(process.env.PORT || 8080);
