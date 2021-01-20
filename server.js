731768b2d4e7039b6ef06d6a4737d157a5b4904e
Leandro Meireles <lmmeireles11@gmail.com>
2021-01-20 10:59:45 -0300

    Primeiro Commit

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/petzappfront"));
+app.use(express.static(__dirname + '/dist/petzappfront'));
 
 
-app.get('/*', (req, res) => {
+app.get('*', function(req, res) {
     
-    res.sendFile(path.join(__dirname + "/dist/petzappfront/index.html"));
+    res.sendFile(path.join(__dirname + '/dist/petzappfront/index.html'));
     });
     
 app.listen(process.env.PORT || 8080);
